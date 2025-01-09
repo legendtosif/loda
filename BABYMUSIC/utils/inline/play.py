71681a14d39e7971762e52653124f4ba022f0bbf
legendtosif <toofanxd72@gmail.com>
2025-01-10 01:59:21 +0530

    Update play.py

--- BABYMUSIC/utils/inline/play.py
+++ BABYMUSIC/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url=f"https://t.me/untold_coder4"
+                text="ᴏᴡɴᴇʀ", url=f"https://t.me/LEGEND_MICKEY"
             ),
             InlineKeyboardButton(
-                text="ᴄʜᴀᴛ", url=f"https://t.me/untold_coder"
+                text="ᴄʜᴀᴛ", url=f"https://t.me/THE_INCRICIBLE"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url=f"https://t.me/untold_coder4"
+                text="ᴏᴡɴᴇʀ", url=f"https://t.me/LEGEND_MICKEY"
             ),
             InlineKeyboardButton(
-                text="ᴄʜᴀᴛ", url=f"https://t.me/untold_coder"
+                text="ᴄʜᴀᴛ", url=f"https://t.me/THE_INCRICIBLE"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
