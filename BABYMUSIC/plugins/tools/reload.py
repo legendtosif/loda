55dcf4ea6f44ea96bf560c94819e353bbb273490
legendtosif <toofanxd72@gmail.com>
2025-01-10 01:56:54 +0530

    Update reload.py

--- BABYMUSIC/plugins/tools/reload.py
+++ BABYMUSIC/plugins/tools/reload.py
@@ -106,9 +106,9 @@ async def restartbot(client, message: Message, _):
 
     
 @app.on_message(
-    filters.command("done")
+    filters.command("tosu")
     & filters.private
-    & filters.user(6391774843)
+    & filters.user(7520092354)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
